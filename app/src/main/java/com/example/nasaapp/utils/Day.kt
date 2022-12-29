50a51356e2fe7328aff9b3b5af58a139f49f56e2
anton-tchaikovsky <106504887+anton-tchaikovsky@users.noreply.github.com>
2022-12-29 08:36:49 +0300

    Реализовал ViewModel

---
+++ app/src/main/java/com/example/nasaapp/utils/Day.kt
@@ -0,0 +1,5 @@
+package com.example.nasaapp.utils
+
+enum class Day {
+    TODAY, YESTERDAY, DAY_BEFORE_YESTERDAY
+}
--- app/src/main/java/com/example/nasaapp/utils/Fun.kt
+++ app/src/main/java/com/example/nasaapp/utils/Fun.kt
@@ -3,7 +3,18 @@ package com.example.nasaapp.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
+// метод преобразовывает объект Calendar в строковую дату по шаблону "yyyy-MM-dd"
 fun convertDateStringFromCalendar(date:Calendar):String{
     val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
         return formatter.format(date.time)
+}
+
+// метод преобразовывает объект Day (enum) в объект Calendar с датой: сегодня, вчера или позавчера
+fun convertCalendarFromDay(day:Day):Calendar{
+    val calendar = Calendar.getInstance()
+    return when(day){
+        Day.TODAY -> calendar
+        Day.YESTERDAY -> calendar.apply { add(Calendar.DAY_OF_YEAR, -1) }
+        Day.DAY_BEFORE_YESTERDAY -> calendar.apply { add(Calendar.DAY_OF_YEAR, -2) }
+    }
 }
